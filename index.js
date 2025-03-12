41dc7d1bd5007c697285813cdbff1086b04f66cf
adabo4 <130857407+adabo4@users.noreply.github.com>
2025-03-12 18:48:48 +0100

    changed package.json

--- index.js
+++ index.js
@@ -152,6 +152,6 @@ app.patch("/post-detail/:id", (req, res) => {
 
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}, http://localhost:${PORT}`) });
